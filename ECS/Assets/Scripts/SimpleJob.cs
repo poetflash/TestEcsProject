73d51ec50d2f686ec3c67365bd6994594af16c84
Andrey <poetflash@gmail.com>
2018-11-14 18:05:15 +0200

    simple job

---
+++ ECS/Assets/Scripts/SimpleJob.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Jobs;
+using UnityEngine;
+
+public struct SimpleJob : IJob
+{
+    public float a, b;
+    
+    public void Execute()
+    {
+        float result = a + b;
+        
+        Debug.Log(result);
+    }
+}
---
+++ ECS/Assets/Scripts/TestJob.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Jobs;
+using UnityEngine;
+
+public class TestJob : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SimpleJob job = new SimpleJob();
+        job.Schedule().Complete();
+    }
+}
